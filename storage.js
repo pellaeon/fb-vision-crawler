ec4c3ac57b3dae40c9cd49da12498d6600bc0f2a
zetavg <mail@zeta.vg>
2019-09-02 22:20:57 +0800

    add getPage for storage

--- storage.js
+++ storage.js
@@ -1,4 +1,5 @@
 const { ExportToCsv } = require('export-to-csv');
+const request = require('request-promise-native');
 
 var testdata = [
 	{
@@ -7,11 +8,11 @@ var testdata = [
 	}
 ];
 
-const options = { 
+const options = {
 	fieldSeparator: ',',
 	quoteStrings: '"',
 	decimalSeparator: '.',
-	showLabels: true, 
+	showLabels: true,
 	showTitle: false,
 	title: 'My Awesome CSV',
 	useTextFile: false,
@@ -21,16 +22,53 @@ const options = {
 
 const csvExporter = new ExportToCsv(options);
 
-
 const putPage = async function (padname, data) {
-	const request = require('request-promise-native');
 	const csvstr = csvExporter.generateCsv(data, true);
-	res = await request({
-		url: 'https://ethercalc.org/_/'+padname,
-		method: "PUT",
+	const responseBody = await request({
+		url: `https://ethercalc.org/_/${padname}`,
+		method: 'PUT',
 		headers: {
 			'Content-Type': 'text/csv'
 		},
 		body: csvstr,
-	}).then( res => { console.log(res) });
+	})
+
+  return responseBody
 }
+
+const getPage = async function (padname) {
+  const responseBody = await request({
+    url: `https://ethercalc.org/_/${padname}/csv.json`,
+    method: 'GET',
+  })
+  const responseData = JSON.parse(responseBody)
+  const [headLine, ...lines] = responseData
+  return lines.map(line => line.reduce((obj, v, i) => (obj[headLine[i]] = v, obj), {}))
+}
+
+module.exports = {
+  putPage,
+  getPage,
+}
+
+const test = async function () {
+  const testdata = [
+    {
+      url: 'https://facebook.com/1',
+      text: '安安，你好！'
+    },
+    {
+      url: 'https://facebook.com/2',
+      text: '注意！\n感謝你的注意！'
+    },
+  ];
+
+  const pageName = 'hello_test_page'
+
+  const putResult = await putPage(pageName, testdata)
+  console.log('putResult', putResult)
+  const getResult = await getPage(pageName)
+  console.log('getResult', getResult)
+}
+
+test()
